891c16f9f74d7f7f7df502f5faafd19054bab662
Zezao1224 <gamer12224@yahoo.com.br>
2023-10-16 22:58:45 -0300

    Adicionar arquivos de projeto.

---
+++ ApiIBGE/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiIBGE.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public string TesteApi()
+        {
+            return "TESTEAPI";
+        }
+
+    }
+}
